2ccb0517ce64595f26019569a62568751bd7e97c
andrewheppner <andrewbheppner@gmail.com>
2015-06-13 16:41:41 -0400

    split up home and away shots and rotated away court

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
 
   var url4 = 'https://probasketballapi.com/games/?api_key=' + api_key ;
 
+  var gamesObject = {};
+
   var teamsObject = {};
 
   var playersObject = {};
@@ -25,18 +27,34 @@ $(document).ready(function() {
     });
   });
   
-  function renderShotData(data) {   
+  function renderShotData(data) { 
     var raw_x = data.loc_x;
     var raw_y = data.loc_y;
     var x = raw_x + 250;
     var y = raw_y + 10
     var type_of_shot = data.action_type;
-    var shot = '<div title="' + type_of_shot + '"></div>';
-    if (data.shot_made === 1){
-    $('#shots').append($(shot).css({top: y, left: x}).addClass('madeshot'));
+    // var team = data.team_id;
+    var shot = ('<div title="' + type_of_shot + '"></div>');
+    // $(shot).data('team', team);
+    // console.log($(shot).data('team'));
+    
+    if(data.opponent_id != gamesObject[data.game_id].home) {
+
+      if (data.shot_made === 1){
+      $('#shots-home').append($(shot).css({top: y, left: x}).addClass('madeshot'));
+      } else {
+      $('#shots-home').append($(shot).css({top: y, left: x}).addClass('missedshot'));
+      } 
+    
     } else {
-    $('#shots').append($(shot).css({top: y, left: x}).addClass('missedshot'));
-    }  
+
+       if (data.shot_made === 1){
+      $('#shots-away').append($(shot).css({top: y, left: x}).addClass('madeshot'));
+      } else {
+      $('#shots-away').append($(shot).css({top: y, left: x}).addClass('missedshot'));
+      }
+    }
+
   };
 
   function createPlayersObject(data){
@@ -53,6 +71,14 @@ $(document).ready(function() {
       var date = data.date;
       var season = data.season;
       var gameId = data.game_id;
+
+      gamesObject[gameId] = {
+        'home': home,
+        'away': away,
+        'season': season,
+        'date': date,
+      };
+
       var gameDiv = ('<div class="game">' + season + ' ' +  
         teamsObject[home] + ' vs. ' + teamsObject[away] + 
          '</div>');
@@ -62,7 +88,7 @@ $(document).ready(function() {
   
   //ON click of .game div, make ajax request to get shot Data
     $('#games').on('click', '.game', function(){
-      $('#shots').empty();
+      $('#shots-home, #shots-away').empty();
       getShotChart($(this).data('gameId'));
     });
   
@@ -81,7 +107,6 @@ $(document).ready(function() {
       //   get_extra: 1
       // },
       success: function(data){
-        console.log(data);
         _.each(data, renderShotData);
           },
       
